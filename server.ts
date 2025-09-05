e7309b62b2c7532829ff2ed026aba3853ef3bd89
Prince Thakur <prince@example.com>
2025-09-06 00:02:11 +0530

    ad databse setup

--- server.ts
+++ server.ts
@@ -1,8 +1,13 @@
 import app from './src/app';
-import {config} from './src/config/config'
+import {config} from './src/config/config';
+import connectDB from './src/config/db';
 
 
-const startServer = () => {
+const startServer = async () => {
+  //connect database
+  await connectDB();
+
+
   const port = config.port || 3000;
 
 
---
+++ src/config/db.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import {config} from "./config";
+
+const connectDB = async ()=>{
+  try {
+    mongoose.connection.on('connected', ()=>{
+      console.log("connected to databse successfully ");
+    })
+
+    mongoose.connection.on('error',(err)=>{
+      console.log("error in connecting to databse", err);
+      
+    })
+
+    
+    await mongoose.connect(config.databaseUrl as string);
+
+    
+  } catch (error) {
+    console.error("failed to connect to database ", error);
+    process.exit(1)
+  }
+}
+
+export default connectDB;
