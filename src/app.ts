6fdbe22b4cbe9af1b16c02fbd6235064573ee743
Prince Thakur <prince@example.com>
2025-09-16 18:57:31 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import globalErrorHandler from './middlewares/globalErrorHandler';
 import userRouter from './user/userRouter';
 import bookRouter from './book/bookRouter';
+import { config } from './config/config';
 
 
 const app = express();
+
+app.use(cors({
+  origin:config.frontend_domain,
+})) // it says allow all the request
 
 app.use(express.json());
 
