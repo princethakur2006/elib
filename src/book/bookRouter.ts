c013f3f22fbf7afb012b86d664d4db8c4a484c01
Prince Thakur <prince@example.com>
2025-09-16 17:21:18 +0530

    add update book and list book endpoint

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import express from 'express';
-import { createBook } from './bookController';
+import { createBook, listBooks, updateBook } from './bookController';
 import multer from 'multer';
 import authenticate from '../middlewares/authenticate';
 
@@ -29,5 +29,14 @@ bookRouter.post(
 ]), createBook); // fields for multiple files , single for single file
 
 
+bookRouter.patch(
+  '/:bookId', 
+  authenticate,
+  upload.fields([
+  {name: 'coverImage', maxCount: 1},
+  {name: 'file', maxCount: 1}
+]), updateBook); // fields for multiple files , single for single file
+
+bookRouter.get('/', listBooks)
 
 export default bookRouter;
